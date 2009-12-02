7d79398c57a57ebd49b79f4206725835f45188cb
bart@bartbes.ath.cx <bart@bartbes.ath.cx>
2009-12-02 16:05:27 +0100

    Fixed incorrect number of arguments expected for contact:getVelocity() :getNormal() and :getSeparation()

--- src/modules/physics/box2d/Contact.cpp
+++ src/modules/physics/box2d/Contact.cpp
@@ -1,14 +1,14 @@
 /**
 * Copyright (c) 2006-2009 LOVE Development Team
-* 
+*
 * This software is provided 'as-is', without any express or implied
 * warranty.  In no event will the authors be held liable for any damages
 * arising from the use of this software.
-* 
+*
 * Permission is granted to anyone to use this software for any purpose,
 * including commercial applications, and to alter it and redistribute it
 * freely, subject to the following restrictions:
-* 
+*
 * 1. The origin of this software must not be misrepresented; you must not
 *    claim that you wrote the original software. If you use this software
 *    in a product, an acknowledgment in the product documentation would be
@@ -40,7 +40,7 @@ namespace box2d
 
 	int Contact::getPosition(lua_State * L)
 	{
-		love::luax_assert_argc(L, 0, 0);
+		love::luax_assert_argc(L, 1, 1);
 		lua_pushnumber(L, world->scaleUp(point.position.x));
 		lua_pushnumber(L, world->scaleUp(point.position.y));
 		return 2;
@@ -48,7 +48,7 @@ namespace box2d
 
 	int Contact::getVelocity(lua_State * L)
 	{
-		love::luax_assert_argc(L, 0, 0);
+		love::luax_assert_argc(L, 1, 1);
 		lua_pushnumber(L, world->scaleUp(point.velocity.x));
 		lua_pushnumber(L, world->scaleUp(point.velocity.y));
 		return 2;
@@ -56,7 +56,7 @@ namespace box2d
 
 	int Contact::getNormal(lua_State * L)
 	{
-		love::luax_assert_argc(L, 0, 0);
+		love::luax_assert_argc(L, 1, 1);
 		lua_pushnumber(L, world->scaleUp(point.normal.x));
 		lua_pushnumber(L, world->scaleUp(point.normal.y));
 		return 2;
