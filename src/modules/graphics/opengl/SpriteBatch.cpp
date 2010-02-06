b94b5e6a9d95aff96d10c24841ce6436faed54f5
rude <none@none>
2010-02-06 13:50:52 +0100

    Fixed SpriteBatch using the wrong type for GL_ELEMENT_ARRAY_BUFFER.

--- src/modules/graphics/opengl/SpriteBatch.cpp
+++ src/modules/graphics/opengl/SpriteBatch.cpp
@@ -81,7 +81,7 @@ namespace opengl
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[1]);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*size*6, indices, GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort)*size*6, indices, GL_STATIC_DRAW);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 		return true;
