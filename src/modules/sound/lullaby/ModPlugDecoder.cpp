fe6d168bf1769d4d5f0db44a3416613d43720a57
rude <none@none>
2011-04-07 18:16:44 +0200

    Enable mStereoSeparation/mMaxMixChannels settings in Windows.

--- src/modules/sound/lullaby/ModPlugDecoder.cpp
+++ src/modules/sound/lullaby/ModPlugDecoder.cpp
@@ -41,11 +41,9 @@ namespace lullaby
 
 		// fill with modplug defaults (modplug _memsets_, so we could get
 		// garbage settings when the struct is only partially initialized)
-#ifndef LOVE_WINDOWS
 		// This does not exist yet on Windows.
 		settings.mStereoSeparation = 128;
 		settings.mMaxMixChannels = 32;
-#endif // LOVE_WINDOWS
 		settings.mReverbDepth = 0;
 		settings.mReverbDelay = 0;
 		settings.mBassAmount = 0;
