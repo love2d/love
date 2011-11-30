5fb4f2c63d32c8bf6177f93d1ef6ba7f4202640b
Bart van Strien <bart.bes+projects@gmail.com>
2011-11-30 17:50:41 +0100

    Request ALL the bits

--- src/modules/graphics/opengl/Graphics.cpp
+++ src/modules/graphics/opengl/Graphics.cpp
@@ -165,9 +165,10 @@ namespace opengl
 		// Set caption.
 
 		// Set GL attributes
-		SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 0);
-		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 0);
-		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 0);
+		SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 		SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, (vsync ? 1 : 0));
 		SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 1);
