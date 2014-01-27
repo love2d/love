3a24e73891cef3294fe9430bb7f6f7906f7ef01d
Alex Szpakowski <slime73@gmail.com>
2014-01-27 02:28:38 -0400

    0.9.1’s API is designed to be backwards-compatible with 0.9.0 games (resolves issue #839)

--- src/common/version.h
+++ src/common/version.h
@@ -29,7 +29,7 @@ const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 9;
 const int VERSION_REV = 1;
 const char *VERSION = "0.9.1";
-const char *VERSION_COMPATIBILITY[] =  { VERSION, 0 };
+const char *VERSION_COMPATIBILITY[] =  { VERSION, "0.9.0", 0 };
 const char *VERSION_CODENAME = "Baby Inspector";
 
 } // love
