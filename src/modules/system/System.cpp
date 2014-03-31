38497d23dab5ff55531223b2d8296be7ab45b073
Alex Szpakowski <slime73@gmail.com>
2014-03-31 16:13:05 -0300

    Added love.system.openURL (resolves issue #863)

--- src/modules/system/System.cpp
+++ src/modules/system/System.cpp
@@ -18,7 +18,21 @@
  * 3. This notice may not be removed or altered from any source distribution.
  **/
 
+// LOVE
+#include "common/config.h"
 #include "System.h"
+
+#if defined(LOVE_MACOSX)
+#include <CoreServices/CoreServices.h>
+#elif defined(LOVE_LINUX)
+#include <stdlib.h>
+#include <unistd.h>
+#elif defined(LOVE_WINDOWS)
+#include "common/utf8.h"
+#include <shlobj.h>
+#include <shellapi.h>
+#pragma comment(lib, "shell32.lib")
+#endif
 
 namespace love
 {
@@ -27,15 +41,78 @@ namespace system
 
 std::string System::getOS() const
 {
-#ifdef LOVE_MACOSX
+#if defined(LOVE_MACOSX)
 	return "OS X";
-#elif LOVE_WINDOWS
+#elif defined(LOVE_WINDOWS)
 	return "Windows";
-#elif LOVE_LINUX
+#elif defined(LOVE_LINUX)
 	return "Linux";
 #else
 	return "Unknown";
 #endif
+}
+
+bool System::openURL(const std::string &url) const
+{
+	bool success = false;
+
+#if defined(LOVE_MACOSX)
+
+	// We could be lazy and use system("open " + url), but this is safer.
+	CFURLRef cfurl = CFURLCreateWithBytes(nullptr,
+	                                      (const UInt8 *) url.c_str(),
+	                                      url.length(),
+	                                      kCFStringEncodingUTF8,
+	                                      nullptr);
+
+	success = LSOpenCFURLRef(cfurl, nullptr) == noErr;
+	CFRelease(cfurl);
+
+#elif defined(LOVE_LINUX)
+
+	// Spawn a child process, which we'll replace with xdg-open.
+	pid_t pid = vfork();
+
+	if (pid == 0) // Child process.
+	{
+		// Replace the child process with xdg-open and pass in the URL.
+		execlp("xdg-open", "xdg-open", url.c_str(), nullptr);
+
+		// exec will only return if it errored, so we should exit with non-zero.
+		_exit(1);
+	}
+	else if (pid > 0) // Parent process.
+	{
+		// Wait for xdg-open to complete (or fail.)
+		int status = 0;
+		if (waitpid(pid, &status, 0) == pid)
+			success = (status == 0);
+		else
+			success = false;
+	}
+	else
+	{
+		// vfork() failed.
+		success = false;
+	}
+
+#elif defined(LOVE_WINDOWS)
+
+	// Unicode-aware WinAPI functions don't accept UTF-8, so we need to convert.
+	std::wstring wurl = to_widestr(url);
+
+	HINSTANCE result = ShellExecuteW(nullptr,
+	                                 L"open",
+	                                 wurl.c_str(),
+	                                 nullptr,
+	                                 nullptr,
+	                                 SW_SHOW);
+
+	success = (int) result > 32;
+
+#endif
+
+	return success;
 }
 
 bool System::getConstant(const char *in, System::PowerState &out)
