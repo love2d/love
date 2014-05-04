0372977dfa689598ab1414050b847ad26c8f8f96
Alex Szpakowski <slime73@gmail.com>
2014-05-03 22:21:45 -0300

    Backout changeset 6dd72101871f163ee0e644c751f80773d45f339f
    The change should only be done for GLES, not desktop GL - their specifications differ on this.

--- src/scripts/graphics.lua
+++ src/scripts/graphics.lua
@@ -1368,7 +1368,7 @@ void main() {
 		local vertexcodes = {
 			GLSL_VERSION,
 			GLSL_SYNTAX, GLSL_VERTEX.HEADER, GLSL_UNIFORMS,
-			"#line 1",
+			"#line 0",
 			vertexcode,
 			GLSL_VERTEX.FOOTER,
 		}
@@ -1379,7 +1379,7 @@ void main() {
 		local pixelcodes = {
 			GLSL_VERSION,
 			GLSL_SYNTAX, GLSL_PIXEL.HEADER, GLSL_UNIFORMS,
-			"#line 1",
+			"#line 0",
 			pixelcode,
 			is_multicanvas and GLSL_PIXEL.FOOTER_MULTI_CANVAS or GLSL_PIXEL.FOOTER,
 		}
