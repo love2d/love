738cc0146810764c0a3501625dd5f12c33ebd2f2
Alex Szpakowski <slime73@gmail.com>
2014-06-18 19:05:43 -0300

    Increased version to 0.9.2.

--- src/common/version.h
+++ src/common/version.h
@@ -27,9 +27,9 @@ namespace love
 // Version stuff.
 const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 9;
-const int VERSION_REV = 1;
-const char *VERSION = "0.9.1";
-const char *VERSION_COMPATIBILITY[] =  { VERSION, "0.9.0", 0 };
+const int VERSION_REV = 2;
+const char *VERSION = "0.9.2";
+const char *VERSION_COMPATIBILITY[] =  { VERSION, "0.9.1", "0.9.0", 0 };
 const char *VERSION_CODENAME = "Baby Inspector";
 
 } // love
