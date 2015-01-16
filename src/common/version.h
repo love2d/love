1253e3aae115d1bce55db1984269d77f575b4596
Alex Szpakowski <slime73@gmail.com>
2015-01-16 15:54:18 -0400

    Updated version to 0.10.0.
    --HG--
    branch : minor

--- src/common/version.h
+++ src/common/version.h
@@ -26,10 +26,10 @@ namespace love
 
 // Version stuff.
 const int VERSION_MAJOR = 0;
-const int VERSION_MINOR = 9;
-const int VERSION_REV = 2;
-const char *VERSION = "0.9.2";
-const char *VERSION_COMPATIBILITY[] =  { VERSION, "0.9.1", "0.9.0", 0 };
+const int VERSION_MINOR = 10;
+const int VERSION_REV = 0;
+const char *VERSION = "0.10.0";
+const char *VERSION_COMPATIBILITY[] =  { VERSION, 0 };
 const char *VERSION_CODENAME = "Baby Inspector";
 
 } // love
