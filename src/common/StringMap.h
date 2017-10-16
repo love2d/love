bf1a9344ebe5b6e7688614d20178e4db0db5158b
Bart van Strien <bart.bes+projects@gmail.com>
2017-10-16 14:40:17 +0200

    Reduce template instantiations of std::vector<std::string>
    The new StringMap code for luax_enumerror causes code to be generated for
    std::vector. By using an extern template (an old feature standardised in c++11)
    we can instantiate it once instead, in StringMap.cpp.
    
    --HG--
    branch : minor

--- src/common/StringMap.h
+++ src/common/StringMap.h
@@ -26,6 +26,12 @@
 #include <string>
 #include <vector>
 
+// As StringMap instantiates std::vector<std::string> for instances that use
+// getNames(), we end up with multiple copies in the object files. This
+// declaration means we only emit it once (in StringMap.cpp).
+extern template class std::vector<std::string>;
+extern template decltype(std::vector<std::string>().emplace_back("")) std::vector<std::string>::emplace_back<const char *const&>(const char *const&);
+
 namespace love
 {
 
