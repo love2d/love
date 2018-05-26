b8c9bc38a1c3068e84ffed2341e5ee64effd5933
David K <davidkrawiec@posteo.net>
2018-05-26 09:54:09 +0200

    Renamed arguments in setTransform method. Merge setters in to one lamba
    --HG--
    branch : bodygettransform

--- src/modules/physics/box2d/wrap_Body.cpp
+++ src/modules/physics/box2d/wrap_Body.cpp
@@ -269,11 +269,13 @@ int w_Body_setY(lua_State *L)
 int w_Body_setTransform(lua_State *L)
 {
 	Body *t = luax_checkbody(L, 1);
-	float arg1 = (float)luaL_checknumber(L, 2);
-	float arg2 = (float)luaL_checknumber(L, 3);
-	float arg3 = (float)luaL_checknumber(L, 4);
-	luax_catchexcept(L, [&](){ t->setPosition(arg1, arg2); });
-	luax_catchexcept(L, [&](){ t->setAngle(arg3); });
+	float x = (float)luaL_checknumber(L, 2);
+	float y = (float)luaL_checknumber(L, 3);
+	float angle = (float)luaL_checknumber(L, 4);
+	luax_catchexcept(L, [&](){
+		t->setPosition(x, y);
+		t->setAngle(angle);
+	});
 	return 0;
 }
 
