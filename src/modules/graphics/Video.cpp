932068e77704673519b71106898e9727abfd0e5d
Bart van Strien <bart.bes+projects@gmail.com>
2019-08-11 15:34:00 +0200

    Stop the attached Source when a Video gets destroyed
    Before it would end up paused, but this means the audio playback thread will
    still keep it alive. Since there are no other references, there is no way to
    then free that Source anymore.

--- src/modules/graphics/Video.cpp
+++ src/modules/graphics/Video.cpp
@@ -96,6 +96,8 @@ Video::Video(Graphics *gfx, love::video::VideoStream *stream, float dpiscale)
 
 Video::~Video()
 {
+	if (source)
+		source->stop();
 }
 
 love::video::VideoStream *Video::getStream()
