4136da52e91863992a732f8c2607732d5a5ae418
Sasha Szpakowski <slime73@gmail.com>
2022-12-04 17:48:17 -0400

    vulkan: fix love.window.getVSync

--- src/modules/window/sdl/Window.cpp
+++ src/modules/window/sdl/Window.cpp
@@ -1148,6 +1148,11 @@ int Window::getVSync() const
 	}
 #endif
 
+#ifdef LOVE_GRAPHICS_VULKAN
+	if (windowRenderer == love::graphics::RENDERER_VULKAN)
+		return love::graphics::vulkan::Vulkan::getVsync();
+#endif
+
 	return 0;
 }
 
