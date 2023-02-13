02e8acc0d2dd4effa073ee82fb69504254652e96
Sasha Szpakowski <slime73@gmail.com>
2023-02-13 19:37:59 -0400

    Add ByteData methods for getting/setting number values.
    See #1594

--- src/modules/data/wrap_ByteData.cpp
+++ src/modules/data/wrap_ByteData.cpp
@@ -20,6 +20,9 @@
 
 #include "wrap_ByteData.h"
 #include "wrap_Data.h"
+#include "common/config.h"
+
+#include <algorithm>
 
 namespace love
 {
@@ -41,9 +44,182 @@ int w_ByteData_clone(lua_State *L)
 	return 1;
 }
 
+template <typename T>
+int w_ByteData_setT(lua_State *L)
+{
+	ByteData *t = luax_checkbytedata(L, 1);
+	int64 offset = (int64) luaL_checknumber(L, 2);
+
+	bool istable = lua_type(L, 3) == LUA_TTABLE;
+	int nargs = std::max(1, istable ? (int) luax_objlen(L, 3) : lua_gettop(L) - 2);
+
+	if (offset < 0 || offset + sizeof(T) * nargs > t->getSize())
+		return luaL_error(L, "");
+
+	auto data = (T *)((uint8 *) t->getData() + offset);
+
+	if (istable)
+	{
+		for (int i = 0; i < nargs; i++)
+		{
+			lua_rawgeti(L, 3, i + 1);
+			data[i] = (T) luaL_checknumber(L, -1);
+			lua_pop(L, 1);
+		}
+	}
+	else
+	{
+		for (int i = 0; i < nargs; i++)
+			data[i] = (T) luaL_checknumber(L, 3 + i);
+	}
+
+	return 0;
+}
+
+template <typename T>
+int w_ByteData_getT(lua_State *L)
+{
+	ByteData *t = luax_checkbytedata(L, 1);
+	int64 offset = (int64) luaL_checknumber(L, 2);
+	int count = (int) luaL_optinteger(L, 3, 1);
+
+	if (count <= 0)
+		return luaL_error(L, "Invalid count parameter (must be greater than 0)");
+
+	if (offset < 0 || offset + sizeof(T) * count > t->getSize())
+		return luaL_error(L, "The given offset and count parameters don't fit within the ByteData's size.");
+
+	auto data = (const T *)((uint8 *) t->getData() + offset);
+
+	for (int i = 0; i < count; i++)
+		lua_pushnumber(L, (lua_Number) data[i]);
+
+	return count;
+}
+
+int w_ByteData_setFloat(lua_State *L)
+{
+	return w_ByteData_setT<float>(L);
+}
+
+int w_ByteData_setDouble(lua_State *L)
+{
+	return w_ByteData_setT<double>(L);
+}
+
+int w_ByteData_setInt8(lua_State *L)
+{
+	return w_ByteData_setT<int8>(L);
+}
+
+int w_ByteData_setUInt8(lua_State *L)
+{
+	return w_ByteData_setT<uint8>(L);
+}
+
+int w_ByteData_setInt16(lua_State *L)
+{
+	return w_ByteData_setT<int16>(L);
+}
+
+int w_ByteData_setUInt16(lua_State *L)
+{
+	return w_ByteData_setT<uint16>(L);
+}
+
+int w_ByteData_setInt32(lua_State *L)
+{
+	return w_ByteData_setT<int32>(L);
+}
+
+int w_ByteData_setUInt32(lua_State *L)
+{
+	return w_ByteData_setT<uint32>(L);
+}
+
+int w_ByteData_setInt64(lua_State *L)
+{
+	return w_ByteData_setT<int64>(L);
+}
+
+int w_ByteData_setUInt64(lua_State *L)
+{
+	return w_ByteData_setT<uint64>(L);
+}
+
+int w_ByteData_getFloat(lua_State *L)
+{
+	return w_ByteData_getT<float>(L);
+}
+
+int w_ByteData_getDouble(lua_State *L)
+{
+	return w_ByteData_getT<double>(L);
+}
+
+int w_ByteData_getInt8(lua_State *L)
+{
+	return w_ByteData_getT<int8>(L);
+}
+
+int w_ByteData_getUInt8(lua_State *L)
+{
+	return w_ByteData_getT<uint8>(L);
+}
+
+int w_ByteData_getInt16(lua_State *L)
+{
+	return w_ByteData_getT<int16>(L);
+}
+
+int w_ByteData_getUInt16(lua_State *L)
+{
+	return w_ByteData_getT<uint16>(L);
+}
+
+int w_ByteData_getInt32(lua_State *L)
+{
+	return w_ByteData_getT<int32>(L);
+}
+
+int w_ByteData_getUInt32(lua_State *L)
+{
+	return w_ByteData_getT<uint32>(L);
+}
+
+int w_ByteData_getInt64(lua_State *L)
+{
+	return w_ByteData_getT<int64>(L);
+}
+
+int w_ByteData_getUInt64(lua_State *L)
+{
+	return w_ByteData_getT<uint64>(L);
+}
+
 static const luaL_Reg w_ByteData_functions[] =
 {
 	{ "clone", w_ByteData_clone },
+	{ "setFloat", w_ByteData_setFloat },
+	{ "setDouble", w_ByteData_setDouble },
+	{ "setInt8", w_ByteData_setInt8 },
+	{ "setUInt8", w_ByteData_setUInt8 },
+	{ "setInt16", w_ByteData_setInt16 },
+	{ "setUInt16", w_ByteData_setUInt16 },
+	{ "setInt32", w_ByteData_setInt32 },
+	{ "setUInt32", w_ByteData_setUInt32 },
+	{ "setInt64", w_ByteData_setInt64 },
+	{ "setUInt64", w_ByteData_setUInt64 },
+	{ "getFloat", w_ByteData_getFloat },
+	{ "getDouble", w_ByteData_getDouble },
+	{ "getInt8", w_ByteData_getInt8 },
+	{ "getUInt8", w_ByteData_getUInt8 },
+	{ "getInt16", w_ByteData_getInt16 },
+	{ "getUInt16", w_ByteData_getUInt16 },
+	{ "getInt32", w_ByteData_getInt32 },
+	{ "getUInt32", w_ByteData_getUInt32 },
+	{ "getInt64", w_ByteData_getInt64 },
+	{ "getUInt64", w_ByteData_getUInt64 },
 	{ 0, 0 }
 };
 
