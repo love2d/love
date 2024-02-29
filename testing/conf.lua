c7e7565a3cd67b5fcd8dc8cd4c01be7cf3f061f8
ell <77150506+ellraiser@users.noreply.github.com>
2024-02-29 17:49:24 +0000

    indentation

--- testing/conf.lua
+++ testing/conf.lua
@@ -13,10 +13,9 @@ end
 -- custom crash message here to catch anything that might occur with modules 
 -- loading before we hit main.lua
 local function error_printer(msg, layer)
-	print((debug.traceback("Error: " .. tostring(msg), 1+(layer or 1)):gsub("\n[^\n]+$", "")))
+  print((debug.traceback("Error: " .. tostring(msg), 1+(layer or 1)):gsub("\n[^\n]+$", "")))
 end
 function love.errorhandler(msg)
-	msg = tostring(msg)
-	error_printer(msg, 2)
-  return nil
+  msg = tostring(msg)
+  error_printer(msg, 2)
 end
