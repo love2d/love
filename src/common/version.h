5ac9b2f1c49c251790b1a8e7987d4af2114f3b91
Sasha Szpakowski <slime73@gmail.com>
2025-01-13 20:30:43 -0400

    Add codename

--- src/common/version.h
+++ src/common/version.h
@@ -31,7 +31,7 @@ static const int VERSION_MINOR = 0;
 static const int VERSION_REV = 0;
 static const char *VERSION = LOVE_VERSION_STRING;
 static const char *VERSION_COMPATIBILITY[] =  { VERSION, 0 };
-static const char *VERSION_CODENAME = "";
+static const char *VERSION_CODENAME = "Bestest Friend";
 
 } // love
 
